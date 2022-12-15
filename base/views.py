a7fd32d51e0a20051529f95b61200b91697e7d2a
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2022-12-15 12:23:42 +0100

    message

--- base/views.py
+++ base/views.py
@@ -29,7 +29,8 @@ def view_csv_detail_page(request, pk):
     csv_file = File.objects.get(id=pk)
     print(csv_file.file.url)
     # df = pd.read_csv("https://excelcomp.up.railway.app/" + csv_file.file.url)
-    df = pd.read_csv("http://127.0.0.1:8000/" + csv_file.file.url)
+    # df = pd.read_csv("http://127.0.0.1:8000/" + csv_file.file.url)
+    df = pd.read_csv("https://excelcomp.up.railway.app/" + csv_file.file.url)
     # print(df)
     context = {
         'df': df.to_html()
